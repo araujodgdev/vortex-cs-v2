af54c19e941603a6cd2d625228916551e5ae4ec4
carlos <carlos.cvlnt@gmail.com>
2025-06-05 23:37:57 -0300

    fix: Ajusta erros de Deploy

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -1,13 +1,7 @@
 import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps as NextThemesProviderProps } from "next-themes"
 
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: string
-  defaultTheme?: string
-  enableSystem?: boolean
-  storageKey?: string 
-}
+type ThemeProviderProps = NextThemesProviderProps
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
